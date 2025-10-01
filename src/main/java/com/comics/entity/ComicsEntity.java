f35b564451a39387580042eaaedfc36a03eb3580
PathmaShree <146501513+PathmaShree@users.noreply.github.com>
2025-10-01 17:57:08 +0530

    Update ComicsEntity.java

--- src/main/java/com/comics/entity/ComicsEntity.java
+++ src/main/java/com/comics/entity/ComicsEntity.java
@@ -12,7 +12,8 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "COMICS")
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 
@@ -25,83 +26,6 @@ public class ComicsEntity {
 	@Column (name = "COMIC_NAME")
 	private String comicName;
 	
-	public ComicsEntity(int comicId, String comicName, String volume, String genere, String author,
-			LocalDate releaseDate, String season, char merch) {
-		super();
-		this.comicId = comicId;
-		this.comicName = comicName;
-		this.volume = volume;
-		this.genere = genere;
-		this.author = author;
-		this.releaseDate = releaseDate;
-		this.season = season;
-		this.merch = merch;
-	}
-
-	public int getComicId() {
-		return comicId;
-	}
-
-	public void setComicId(int comicId) {
-		this.comicId = comicId;
-	}
-
-	public String getComicName() {
-		return comicName;
-	}
-
-	public void setComicName(String comicName) {
-		this.comicName = comicName;
-	}
-
-	public String getVolume() {
-		return volume;
-	}
-
-	public void setVolume(String volume) {
-		this.volume = volume;
-	}
-
-	public String getGenere() {
-		return genere;
-	}
-
-	public void setGenere(String genere) {
-		this.genere = genere;
-	}
-
-	public String getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-
-	public LocalDate getReleaseDate() {
-		return releaseDate;
-	}
-
-	public void setReleaseDate(LocalDate releaseDate) {
-		this.releaseDate = releaseDate;
-	}
-
-	public String getSeason() {
-		return season;
-	}
-
-	public void setSeason(String season) {
-		this.season = season;
-	}
-
-	public char getMerch() {
-		return merch;
-	}
-
-	public void setMerch(char merch) {
-		this.merch = merch;
-	}
-
 	@Column (name = "VOLUME")
 	private String volume;
 	
